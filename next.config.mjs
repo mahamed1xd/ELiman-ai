97c858e7d6a7e84f71cde145e2f706bd1bb49478
Mohamed <mahamed1xd@gmail.com>
2025-11-06 14:58:14 +0200

    feat: Integrate botid package and update next.config for improved functionality

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,10 @@
-import withPWAInit from 'next-pwa'
+import { withBotId } from 'botid/next/config'
 
-const withPWA = withPWAInit({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-export default withPWA(nextConfig)
+// الدمج الصحيح بين الإضافتين
+export default withBotId(nextConfig)
