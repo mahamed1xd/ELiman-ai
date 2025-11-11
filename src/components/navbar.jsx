f27ae752f9c023b7d0d4e65a5ce093604725d50c
Mohamed <mahamed1xd@gmail.com>
2025-11-11 23:25:06 +0200

    edit some

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useAuth } from "@/context/authContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faCommentDots, faStarAndCrescent, faBookQuran } from "@fortawesome/free-solid-svg-icons";
+
 
 
 export default function NavbarComponent({ children }) {
@@ -171,22 +174,22 @@ export default function NavbarComponent({ children }) {
 
       <div className="dock bg-neutral lg:hidden bottom-[-1px] w-[101%] text-neutral-content">
         <button className={activePage === "home" ? "dock-active" : ""} id="home" onClick={() => switching('home')}>
-          <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="currentColor" strokeLinejoin="miter" strokeLinecap="butt"><polyline points="1 11 12 2 23 11" fill="none" stroke="currentColor" strokeMiterlimit="10" strokeWidth="2"></polyline><path d="m5,13v7c0,1.105.895,2,2,2h10c1.105,0,2-.895,2-2v-7" fill="none" stroke="currentColor" strokeLinecap="square" strokeMiterlimit="10" strokeWidth="2"></path><line x1="12" y1="22" x2="12" y2="18" fill="none" stroke="currentColor" strokeLinecap="square" strokeMiterlimit="10" strokeWidth="2"></line></g></svg>
+          <FontAwesomeIcon icon={faHome} className="size-[1.2em]" />
           <span className="dock-label">Home</span>
         </button>
 
         <button id="azkar" className={activePage === "azkar" ? "dock-active" : ""} onClick={() => switching('azkar')}>
-          <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="currentColor" strokeLinejoin="miter" strokeLinecap="butt"><polyline points="3 14 9 14 9 17 15 17 15 14 21 14" fill="none" stroke="currentColor" strokeMiterlimit="10" strokeWidth="2"></polyline><rect x="3" y="3" width="18" height="18" rx="2" ry="2" fill="none" stroke="currentColor" strokeLinecap="square" strokeMiterlimit="10" strokeWidth="2"></rect></g></svg>
+          <FontAwesomeIcon icon={faStarAndCrescent} className="size-[1.2em]" />
           <span className="dock-label">Azkar</span>
         </button>
         <button className={activePage === "quran" ? "dock-active" : ""} id="quran" onClick={() => switching('quran')}>
-          <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="currentColor" strokeLinejoin="miter" strokeLinecap="butt"><polyline points="3 14 9 14 9 17 15 17 15 14 21 14" fill="none" stroke="currentColor" strokeMiterlimit="10" strokeWidth="2"></polyline><rect x="3" y="3" width="18" height="18" rx="2" ry="2" fill="none" stroke="currentColor" strokeLinecap="square" strokeMiterlimit="10" strokeWidth="2"></rect></g></svg>
+          <FontAwesomeIcon icon={faBookQuran} className="size-[1.2em]" />
           <span className="dock-label">Quran</span>
         </button>
 
 
         <button id="ai" className={activePage === "ai" ? "dock-active" : ""} onClick={() => switching('ai')}>
-          <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="currentColor" strokeLinejoin="miter" strokeLinecap="butt"><circle cx="12" cy="12" r="3" fill="none" stroke="currentColor" strokeLinecap="square" strokeMiterlimit="10" strokeWidth="2"></circle><path d="m22,13.25v-2.5l-2.318-.966c-.167-.581-.395-1.135-.682-1.654l.954-2.318-1.768-1.768-2.318.954c-.518-.287-1.073-.515-1.654-.682l-.966-2.318h-2.5l-.966,2.318c-.581.167-1.135.395-1.654.682l-2.318-.954-1.768,1.768.954,2.318c-.287.518-.515,1.073-.682,1.654l-2.318.966v2.5l2.318.966c.167.581.395,1.135.682,1.654l-.954,2.318,1.768,1.768,2.318-.954c.518.287,1.073.515,1.654.682l.966,2.318h2.5l.966-2.318c.581-.167,1.135-.395,1.654-.682l2.318.954,1.768-1.768-.954-2.318c.287-.518.515-1.073.682-1.654l2.318-.966Z" fill="none" stroke="currentColor" strokeLinecap="square" strokeMiterlimit="10" strokeWidth="2"></path></g></svg>
+          <FontAwesomeIcon icon={faCommentDots} className="size-[1.2em]" />
           <span className="dock-label">Ai chat</span>
         </button>
       </div>
